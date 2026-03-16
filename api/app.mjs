32bf110673610ae5c89f453d576231ee00bbbf77
gris2004 <bautistatris2@gmail.com>
2026-03-16 16:14:38 -0600

    feat(api/controllers/recordsController): a new class called RecordsController - functions: insertrecord, describeTable, updaterecord

--- api/app.mjs
+++ api/app.mjs
@@ -4,6 +4,10 @@ import dotenv from 'dotenv'
 const app = express();
 dotenv.config();
 
+app.use(express.json());
+//app.use('/api', router)
+app.set('json spaces', 2);
+
 const port = process.env.API_PORT || 8080;
 const ip = process.env.LOCAL_IP || localhost;
 
